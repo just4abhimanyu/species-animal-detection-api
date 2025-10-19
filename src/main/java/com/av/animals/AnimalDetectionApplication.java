838a9b422f3f3c08aebe8fdc685e3b0ec4c81c52
Abhimanyu Kumar <abhimanyu392kumar@gmail.com>
2025-10-20 00:00:58 +0530

    swagger implementation

--- src/main/java/com/av/animals/AnimalDetectionApplication.java
+++ src/main/java/com/av/animals/AnimalDetectionApplication.java
@@ -2,8 +2,11 @@ package com.av.animals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Java Animal Service API", version = "1.0", description = "Java service OpenAPI docs"))
 public class AnimalDetectionApplication {
 
     public static void main(String[] args) {
